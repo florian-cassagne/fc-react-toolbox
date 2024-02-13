a3db9bf28cc4243f53479dc204f3a6ef4a2f5d3d
Florian CASSAGNE <florian.cassagne.pro@gmail.com>
2024-02-13 17:45:11 +0100

    Update 13-2-24

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// Importez chaque composant
+import ButtonLink from './components/Button/ButtonLink';
+import CarouselBase from './components/Carousel/CarouselBase';
+// ... importez tous les autres composants nécessaires
+
+// Exportez chaque composant
+export { ButtonLink, CarouselBase };
+// ... exportez tous les autres composants
