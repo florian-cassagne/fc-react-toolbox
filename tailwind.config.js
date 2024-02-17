cba3f70ce36848ef4f515a4f851f5a822d4e212c
Florian CASSAGNE <florian.cassagne.pro@gmail.com>
2024-02-17 21:50:45 +0100

    Added : output CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     extend: {},
   },
   purge: {
-    // ...
     options: {
       safelist: [
         'bg-red-800', 'bg-orange-800', 'bg-yellow-800', 'bg-green-800', 'bg-teal-800', 'bg-blue-800', 'bg-indigo-800', 'bg-purple-800', 'bg-pink-800',
