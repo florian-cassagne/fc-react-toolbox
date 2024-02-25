bece693e171af154df94191d05f681fefc6f3172
Florian CASSAGNE <florian.cassagne.pro@gmail.com>
2024-02-25 12:08:45 +0100

    Update 25-2-24

--- src/components/Miscellaneous/ScrollToTop.jsx
+++ src/components/Miscellaneous/ScrollToTop.jsx
@@ -1,14 +1,13 @@
 import {useEffect} from "react";
-import {useLocation} from "react-router-dom";
 
 
-function ScrollToTop(DOMelementId) {
-  const pathname = useLocation();
+function ScrollToTop(DOMelementId, currentPathname) {
+  //const pathname = useLocation();
   useEffect(() => {
     //TODO Fix bug when DOMelementId is used instead of 'header'
     const DOMheaderHeight = document.getElementById('header').offsetHeight
     window.scrollTo(0, DOMheaderHeight)
-  }, [pathname])
+  }, [currentPathname])
 }
 
 
